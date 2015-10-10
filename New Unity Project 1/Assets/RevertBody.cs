e31a3ca2a2c811e40b4d307d5773a82658565ced
Kotshi <gildas.picou@epitech.eu>
2015-10-10 12:14:17 +0200

    Coucou
    Yalatetitourne

---
+++ New Unity Project 1/Assets/RevertBody.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevertBody : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(Input.mousePosition.x);
+        print(Screen.width / 2);
+        if (Input.mousePosition.x < (Screen.width / 2))
+            gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
+        else
+            gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+}
---
+++ New Unity Project 1/Assets/RotateHead.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateHead : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float[] PlayerPos = new float[] { Screen.width / 2, Screen.height / 2 };
+        float rotation;
+
+        rotation = Mathf.Atan((Input.mousePosition.y - PlayerPos[1]) / Mathf.Abs(Input.mousePosition.x - PlayerPos[0])) * (180 / Mathf.PI);
+        gameObject.transform.eulerAngles = new Vector3(0, 0, rotation);
+    }
+}
