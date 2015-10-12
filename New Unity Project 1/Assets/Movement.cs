158b722e4b764e5617a6374f65597963cac75ccb
Kotshi <gildas.picou@epitech.eu>
2015-10-12 21:34:16 +0200

    Jump
    Saut dosable

--- New Unity Project 1/Assets/Movement.cs
+++ New Unity Project 1/Assets/Movement.cs
@@ -6,23 +6,27 @@ public class Movement : MonoBehaviour {
     public int Accelleration;
     public int JumpSpeed;
     public int JumpAmount;
+    public float JumpLength;
+    private float JumpStart;
     private GroundCheck children;
     private Rigidbody2D rg2d;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         children = gameObject.GetComponentInChildren<GroundCheck>();
         children.JumpCount = JumpAmount;
         rg2d = gameObject.GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
         if (rg2d.velocity.x < Speed)
             rg2d.AddForce(Vector2.right * Accelleration * Input.GetAxis("Horizontal"));
-        if (Input.GetButtonDown("Jump") && children.JumpCount < JumpAmount)
+        if (Input.GetButtonDown("Jump"))
+            JumpStart = Time.time;
+        if (Input.GetButton("Jump") && children.JumpCount < JumpAmount && Time.time - JumpStart < JumpLength)
             rg2d.velocity = new Vector2(rg2d.velocity.x, JumpSpeed);
         if (Input.GetButtonUp("Jump"))
             children.JumpCount++;
-         }
+    }
 }
